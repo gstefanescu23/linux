869b84c825ba4201dd7403d3699209e35d238809
gstefanescu23 <georgiana.stefanescu04@gmail.com>
2022-04-07 22:20:29 +0300

    LAB05: commit skels
    Signed-off-by: gstefanescu23 <georgiana.stefanescu04@gmail.com>

--- tools/labs/skels/deferred_work/1-2-timer/timer.c
+++ tools/labs/skels/deferred_work/1-2-timer/timer.c
@@ -20,12 +20,8 @@ static struct timer_list timer;
 static void timer_handler(struct timer_list *tl)
 {
 	/* TODO 1: print a message */
-	static size_t sec;
-	sec = sec + TIMER_TIMEOUT;
-	pr_info("seconds: %d\n", sec);
 
 	/* TODO 2: rechedule timer */
-	mod_timer(tl, jiffies + TIMER_TIMEOUT * HZ);
 }
 
 static int __init timer_init(void)
@@ -33,10 +29,8 @@ static int __init timer_init(void)
 	pr_info("[timer_init] Init module\n");
 
 	/* TODO 1: initialize timer */
-	timer_setup(&timer, timer_handler, 0);
 
 	/* TODO 1: schedule timer for the first time */
-	mod_timer(&timer, jiffies + TIMER_TIMEOUT * HZ);
 
 	return 0;
 }
@@ -46,7 +40,6 @@ static void __exit timer_exit(void)
 	pr_info("[timer_exit] Exit module\n");
 
 	/* TODO 1: cleanup; make sure the timer is not running after we exit */
-	del_timer_sync(&timer);
 }
 
 module_init(timer_init);
