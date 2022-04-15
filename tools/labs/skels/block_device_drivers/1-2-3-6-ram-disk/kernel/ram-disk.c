ce32677bdaf6592c25b7855aea7ea746377fbee4
gstefanescu23 <georgiana.stefanescu04@gmail.com>
2022-04-15 09:11:29 +0300

    lab07: ex1: register/ unregisted block device
    Signed-off-by: gstefanescu23 <georgiana.stefanescu04@gmail.com>

--- tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
+++ tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
@@ -179,6 +179,13 @@ static int __init my_block_init(void)
 	int err = 0;
 
 	/* TODO 1: register block device */
+	int status;
+
+    	status = register_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
+    	if (status < 0) {
+             printk(KERN_ERR "unable to register mybdev block device\n");
+             return -EBUSY;
+     	}
 
 	/* TODO 2: create block device using create_block_device */
 
@@ -209,6 +216,7 @@ static void __exit my_block_exit(void)
 	/* TODO 2: cleanup block device using delete_block_device */
 
 	/* TODO 1: unregister block device */
+	unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
 }
 
 module_init(my_block_init);
